aab1afbac0c6e89e479819513f7a54ba5331b8df
aananthv <aananthv@outlook.com>
2020-02-08 00:14:10 +0530

    Add Routes

---
+++ controllers/authController.js
@@ -0,0 +1,11 @@
+exports.studentLogin = (req, res, next) => {
+    res.send('Lalalalal')
+}
+
+exports.vendorLogin = (req, res, next) => {
+    res.send('Lalalalal')
+}
+
+exports.validateJWT = (req, res, next) => {
+    res.next()
+}
---
+++ controllers/paymentController.js
@@ -0,0 +1,23 @@
+exports.createPayment = (req, res, next) => {
+    res.send('Lalalalal')
+}
+
+exports.registerPayment = (req, res, next) => {
+    res.send('Lalalalal')
+}
+
+exports.cancelPayment = (req, res, next) => {
+    res.send('Lalalalal')
+}
+
+exports.denyPayment = (req, res, next) => {
+    res.send('Lalalalal')
+}
+
+exports.pendingRequests = (req, res, next) => {
+    res.send('Lalalalal')
+}
+
+exports.pendingPayments = (req, res, next) => {
+    res.send('Lalalalal')
+}
