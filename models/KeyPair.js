5d06e5a8f277e2b110bf64f1fda1a8603db3a5f6
Balaji Jayashrri <balaji.jayashrri@gmail.com>
2020-02-09 00:30:34 +0530

    Modify payment routes

--- models/KeyPair.js
+++ models/KeyPair.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose')
 
 const keyPairSchema = new mongoose.Schema({
   peer_public_key: String,
-  private_key: String
+  private_key: String,
+  userId: String
 });
 
 module.exports = mongoose.model('KeyPair', keyPairSchema)
